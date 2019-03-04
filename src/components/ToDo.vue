bec817e31e9c8acec888eea02d5d4e9f44ae5fee
weffelms <stacie.weffelmeyer@wwt.com>
2019-03-04 10:22:20 -0600

    connected to mongo added and removing todos

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,8 +1,13 @@
 <template lang="html">
   <div>
+      <form v-on:submit='addTodo($event)'>
+          <input type='text' placeholder='Enter Todo' v-model='newTodo' />
+          <input type='submit' />
+      </form>
     <ul>
-      <li v-for='todo in todos' :key="todo">
-        <span>{{todo}}</span>
+      <li v-for='todo in todos' :key="todo._id">
+          <input type='checkbox' @click='deleteTodo(todo._id)' />
+        <span>{{todo.title}}</span>
       </li>
     </ul>
   </div>
@@ -12,7 +17,8 @@ import ToDoAPI from '@/services/ToDoAPI.js'
 export default {
   data () {
     return {
-      todos: []
+      todos: [],
+      newTodo: ''
     }
   },
   mounted () {
@@ -22,6 +28,22 @@ export default {
     async loadTodos () {
       const response = await ToDoAPI.getToDos()
       this.todos = response.data
+    },
+    async addTodo (evt) {
+      evt.preventDefault() // prevents the form's default action from redirecting the page
+      console.log('evt', evt)
+      console.log('new todo', this.newTodo)
+      const response = await ToDoAPI.addTodo(this.newTodo)
+      console.log('response--', response)
+      this.todos.push(response.data)
+      this.newTodo = '' // clear the input field
+    },
+    deleteTodo (todoID) {
+      ToDoAPI.deleteTodo(todoID)
+      //  remove the array element with the matching id
+      this.todos = this.todos.filter(function (obj) {
+        return obj._id !== todoID
+      })
     }
   }
 }
